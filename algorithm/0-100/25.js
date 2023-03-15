12e1337a9fc4adf364b17dc8141026014bf5bbb4
WJX2001 <1758807220@qq.com>
2023-03-15 23:47:45 +0800

    K个一组反转链表

--- algorithm/0-100/25.js
+++ algorithm/0-100/25.js
@@ -1,33 +1,56 @@
-// 合并两个排序的链表
-// 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+// k个一组 反转链表
+/** 
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 
-var mergeTwoLists = function(l1, l2) {
-    // 虚拟头节点
-    let dummy = new  ListNode(-1), p = dummy;
-    let p1 = l1,p2 = l2
+输入：head = [1,2,3,4,5], k = 2
+输出：[2,1,4,3,5]
 
-    while(p1 != null && p2 !=null) {
-        // 比较p1 p2 两个指针
-        // 将值较小的节点加入到p节点
-        if(p1.val > p2.val){
-            p.next = p2
-            p2 = p2.next
-        } else {
-            p.next = p1
-            p1 = p1.next
-        }
-        // p指针不断前进
-        p = p.next
+输入：head = [1,2,3,4,5], k = 3
+输出：[3,2,1,4,5]
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    if(!head) return null;
+    // 区间[a,b) 包含k个待反转元素
+    let a = head,b = head
+    for(let i=0;i<k;i++) {
+        // 不足k个 不需要反转 base case
+        if(!b) return head
+        b = b.next;
     }
+    // 反转前k个元素
+    let newHead = reverse(a,b);
+    // 递归反转后续链表并连接起来
+    a.next = reverseKGroup(b,k)
+    return newHead
 
-    if (p1 != null) {
-        p.next = p1;
+}
+/**反转区间 [a,b)的元素，注意是左闭右开 */
+var reverse = function(a,b){
+    let pre = null
+    let cur = a,nxt = a
+    
+    // while的终止条件改一下就可以了
+    while(cur !== b){
+        nxt = cur.next
+        cur.next = pre
+        pre = cur
+        cur = nxt;
     }
-
-    if (p2 != null) {
-        p.next = p2;
-    }
-
-    return dummy.next;
+    // 返回反转后的头节点
+    return pre
 
 }
--- algorithm/100-200/104.js
+++ algorithm/100-200/104.js
@@ -34,3 +34,15 @@ function maxDepth(root) {
   }
   return res
 }
+
+
+function maxDepth(root) {
+  // 动态规划思路
+  if(root === null){
+      return 0
+  }
+  const leftMax = maxDepth(root.left)
+  const rightMax = maxDepth(root.right)
+  // 根据左右子树的最大深度推出原二叉树的最大深度
+  return 1+Math.max(leftMax,rightMax)  // 加1的原因是，最大深度要加上根节点
+}
