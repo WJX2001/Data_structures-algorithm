97c0f9bba3b1dbf4946eb953e6831b0497567347
WJX2001 <1758807220@qq.com>
2023-03-25 18:46:06 +0800

    二叉树

---
+++ algorithm/二叉树/104.js
@@ -0,0 +1,18 @@
+// 二叉树的最大深度
+var maxDepth = function (root) {
+    let res = 0
+    let depth = 0
+    var traverse = function(root){
+        if(root === null) return;
+        // 前序位置
+        depth++
+        // 遍历的过程中记录的最大深度
+        res = Math.max(res,depth)
+        traverse(root.left)
+        traverse(root.right)
+        // 后续遍历位置
+        depth--
+    }
+    traverse(root)
+    return res
+}
