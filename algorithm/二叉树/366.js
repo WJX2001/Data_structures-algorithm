f9303f95f1e530950880b16590ad8eda1bafb236
WJX2001 <1758807220@qq.com>
2023-03-25 22:05:31 +0800

    寻找二叉树的叶子节点

--- algorithm/二叉树/366.js
+++ algorithm/二叉树/366.js
@@ -1,26 +1,24 @@
-// 寻找二叉树的叶子节点
-// 这题需要把二叉树的高度理解成从叶子节点到根节点从下到上递增的，那么可以把相同高度的节点分到一起就是答案
+// 注意：javascript 代码由 chatGPT🤖 根据我的 java 代码翻译，旨在帮助不同背景的读者理解算法逻辑。
+// 本代码已经通过力扣的测试用例，应该可直接成功提交。
 
-// 首先计算二叉树的最大高度，然后再函数的后序遍历位置就可以获取当前节点距离叶子节点的高度
 var findLeaves = function(root) {
-    var res = [];
-    
+    var res = []
     var maxDepth = function(root) {
-        if (root === null) {
-            return 0;
+        if(root === null) {
+            return 0
         }
-        // 当前节点距离叶子节点的高度（最大深度）
-        var h = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-        // 后序遍历位置
-        if (res.length < h) {
-            res.push([]);
-        }
-        // 把所有相同高度的叶子节点放在一起
-        res[h - 1].push(root.val);
-        return h;
+        // 当前节点距离叶子节点的高度（最大深度）开始遍历
+        let leftMax = maxDepth(root.left)
+        let rightMax = maxDepth(root.right)
+        var h = Math.max(leftMax,rightMax)+1
+        // 后序遍历位置     
+       if(res.length<h) {
+           res.push([])
+       }
+       // 把所有相同高度的叶子节点放在一起
+       res[h-1].push(root.val)
+       return h
     }
-    
-    maxDepth(root);
-    return res;
-};
-findLeaves([1,2,3,4,5])
+    maxDepth(root)
+    return res
+ };
