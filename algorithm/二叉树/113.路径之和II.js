a94aa843d66a982d464dbf8ea2c568c8d9adea58
WJX2001 <1758807220@qq.com>
2023-04-01 18:17:41 +0800

    路径之和II.js

--- algorithm/二叉树/113.路径之和II.js
+++ algorithm/二叉树/113.路径之和II.js
@@ -2,8 +2,9 @@ var pathSum = function(root, targetSum) {
     let res = []
     let path = []
     function traverse(root,targetSum){
+        if(root === null) return 
+        
         let remain = targetSum - root.val
-
         if (root.left == null && root.right == null && remain == 0) {
             // 找到一条路径
             res.push([...path, root.val]); // 将路径列表和当前节点值合并成新的数组，添加到结果列表中
