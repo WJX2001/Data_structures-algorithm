a8505d46282a609c4593130448387c17e3ad4ddf
WJX2001 <1758807220@qq.com>
2023-04-04 10:19:55 +0800

    二叉树的层序遍历

--- algorithm/二叉树/107.二叉树的层序遍历II.js
+++ algorithm/二叉树/107.二叉树的层序遍历II.js
@@ -6,9 +6,10 @@ var levelOrderBottom = function(root) {
     let q =[]
     q.push(root)
     while(q.length >0){
+        // 记录这一层的节点值
         let level = []
         let sz = q.length
-
+        // for 循环控制每一层从左向右遍历
         for(let i=0;i<sz;i++){
             let cur = q.shift()
             level.push(cur.val)
@@ -20,6 +21,7 @@ var levelOrderBottom = function(root) {
                 q.push(cur.right)
             }
         }
+        // 把每一层添加到头部，就是自底向上的层序遍历
         res.unshift(level)
     }
     return res
